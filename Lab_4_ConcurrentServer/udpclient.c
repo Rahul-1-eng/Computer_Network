db32eb805e2509760e1347eaad91ec7fbb4c6614
Rahul Kumar Sahoo <rahulkumarsahoo1974@gmail.com>
2026-09-04 16:12:13 +0530

    Implement UDP client to send and receive messages

--- Lab_4_ConcurrentServer/udpclient.c
+++ Lab_4_ConcurrentServer/udpclient.c
@@ -1,1 +1,24 @@
 //udpclient.c
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+int main() {
+    int cliSock = socket(AF_INET, SOCK_DGRAM, 0);
+    struct sockaddr_in srvAddr;
+    srvAddr.sin_family = AF_INET;
+    srvAddr.sin_port = htons(8081);
+    inet_pton(AF_INET, "127.0.0.1", &srvAddr.sin_addr);
+
+    char netBuf[256];
+    sendto(cliSock, "STATUS", 6, 0, (struct sockaddr*)&srvAddr, sizeof(srvAddr));
+    
+    socklen_t addrLen = sizeof(srvAddr);
+    int bytes = recvfrom(cliSock, netBuf, 255, 0, (struct sockaddr*)&srvAddr, &addrLen);
+    netBuf[bytes] = '\0';
+    
+    printf("Response:\n%s", netBuf);
+    close(cliSock);
+    return 0;
+}
